747e291bf496ca3843860a5e15be40c21d15c563
MoodyMax <117177874+MoodyMax@users.noreply.github.com>
2023-03-15 15:41:55 -0400

    Add files via upload

---
+++ 13.3 Arrow Function Exercise.js
@@ -0,0 +1,10 @@
+// function double(arr) {
+//     return arr.map(function(val) {
+//         return val * 2;
+//       });
+//     }
+
+const double = (arr) => arr.map((val) => val * 2);
+
+const squareAndFindEvens = (numbers) =>
+  numbers.map(num ** 2).filter(square % 2 === 0);
